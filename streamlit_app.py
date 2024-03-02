aae64985fccfa1c2d5feb5c9969db88285e2d0ce
jsrini-learn1 <161971727+jsrini-learn1@users.noreply.github.com>
2024-03-02 12:48:33 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,14 +33,14 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
-    for ingredient in ingredients_list:
-        ingredients_string += ingredient + ' '
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
 
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + ingredient)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
