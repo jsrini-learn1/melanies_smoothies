63fc79ad2e9c21f67d6184c321ec10a66d9f3632
jsrini-learn1 <161971727+jsrini-learn1@users.noreply.github.com>
2024-03-02 12:47:07 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,11 @@ if ingredients_list:
     ingredients_string = ''
     for ingredient in ingredients_list:
         ingredients_string += ingredient + ' '
+
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + ingredient)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
